2d13d7a2ac0fe99073f48c8b019e656879ae95d4
Maksym Mishchuk <maxim.mishchuk@gmail.com>
2023-11-14 01:23:31 +0200

    Revert "refactored project into RESTful API"
    This reverts commit f5d455674bfd8c716be4ac7e712de827be9c2e33.

--- src/main/java/com/labs/tenderservice/controller/UserController.java
+++ src/main/java/com/labs/tenderservice/controller/UserController.java
@@ -1,56 +1,48 @@
 package com.labs.tenderservice.controller;
 
+import com.labs.tenderservice.entity.tender.Tender;
+import com.labs.tenderservice.entity.tender.TenderURLConnector;
 import com.labs.tenderservice.entity.user.User;
+import com.labs.tenderservice.service.TenderService;
 import com.labs.tenderservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.Collection;
+import java.util.List;
 
-@RestController
-@RequestMapping("/users")
+@Controller
 public class UserController {
     @Autowired
     UserService userService;
 
-    @PostMapping
-    ResponseEntity<User> create(@RequestBody User newUser) {
-        User user = userService.create(newUser);
-        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    @Autowired
+    TenderService tenderService;
+
+    @PostMapping("/user")
+    String createUser(String username, Model model) {
+        userService.createUser(username);
+        model.addAttribute("message", username + " was successfully added!");
+        return "result";
     }
 
-    @GetMapping("/{id}")
-    ResponseEntity<User> getById(@PathVariable long id) {
-        User user = userService.getById(id);
-        if (user == null) {
-            return ResponseEntity.notFound().build();
-        }
-        return ResponseEntity.ok(user);
+    @GetMapping("/user/{userId}/tenders")
+    String showUserTenders(@PathVariable("userId") long userId, Model model) {
+        List<Tender> list = tenderService.getUserTenders(userId);
+        List<TenderURLConnector> urlList = tenderService.getAllURLConnectors();
+        model.addAttribute("tenderList", list);
+        model.addAttribute("urlList", urlList);
+        return "user/userTender";
     }
 
-    @GetMapping
-    ResponseEntity<Collection<User>> getAll() {
-        Collection<User> users = userService.getAll();
-        return ResponseEntity.ok(users);
+    @GetMapping("/user")
+    String showAllUsers(Model model) {
+        List<User> list = userService.getAllUsers();
+        model.addAttribute("userList", list);
+        return "user/allUsers";
     }
 
-    @PutMapping
-    ResponseEntity<User> update(@RequestBody User updatedUser) {
-        User user = userService.update(updatedUser);
-        if (user == null) {
-            return ResponseEntity.notFound().build();
-        }
-        return ResponseEntity.ok(user);
-    }
-
-    @DeleteMapping("/{id}")
-    ResponseEntity<User> delete(@PathVariable long id) {
-        User user =  userService.delete(id);
-        if (user == null) {
-            return ResponseEntity.notFound().build();
-        }
-        return ResponseEntity.ok(user);
-    }
 }
