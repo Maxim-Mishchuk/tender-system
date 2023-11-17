4b22f89c5aefc89cab3e012a11c684c5c2cbfa5a
Maksym Mishchuk <maxim.mishchuk@gmail.com>
2023-11-17 08:40:14 +0200

    small fixes in services

--- src/main/java/com/labs/tenderservice/service/UserService.java
+++ src/main/java/com/labs/tenderservice/service/UserService.java
@@ -4,6 +4,7 @@ import com.labs.tenderservice.entity.user.User;
 import com.labs.tenderservice.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -31,6 +32,7 @@ public class UserService {
         return userRepository.update(changedUser);
     }
 
+    @Transactional
     public User delete(long id) {
         return userRepository.delete(id);
     }
